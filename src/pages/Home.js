67cb08ff196131d0b0f2751a641f80f050b9bae5
PatrikG <Patrik0Gn@gmail.com>
2021-07-26 01:58:57 +0200

    Add logo for home screen

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,16 @@
+import mtg_logo from "../images/mtg_logo.png";
+import "./Home.css";
+
 export default function Home() {
   document.title = "MTG-Card-Search";
-  return <h2>Home</h2>;
+  return (
+    <>
+      <h2>Home</h2>
+      <img
+        className="Home__mtgLogo"
+        src={mtg_logo}
+        alt="Magic the gathering logo"
+      />
+    </>
+  );
 }

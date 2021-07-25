3bb29e89d11176e910986a18bc591771ee20816d
PatrikG <Patrik0Gn@gmail.com>
2021-07-25 17:56:25 +0200

    Implement load more button

--- src/pages/CardListPage.js
+++ src/pages/CardListPage.js
@@ -1,33 +1,46 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import "./CardListPage.css";
 
 export default function CardListPage() {
   const [cards, setCards] = useState([]);
+  const [page, setPage] = useState(582);
 
   useEffect(() => {
-    const url = "https://api.magicthegathering.io/v1/cards?set=DDO";
+    const url = `https://api.magicthegathering.io/v1/cards?page=${page}`;
     fetch(url)
       .then((resp) => resp.json())
       .then((data) => {
         setCards(data.cards);
       });
-  }, []);
+  }, [page]);
 
   function renderCards() {
-    const cardList = cards.map((card) => {
-      return (
-        <li key={card.id}>
-          <Link to={`/cards/${card.id}`}>{card.name}</Link>
-        </li>
-      );
-    });
-    return cardList;
+    if (cards.length === 0) {
+      return <li>Reached end of list.</li>;
+    } else {
+      const cardList = cards.map((card) => {
+        return (
+          <li key={card.id}>
+            <Link to={`/cards/${card.id}`}>{card.name}</Link>
+          </li>
+        );
+      });
+
+      console.log(cards);
+      return cardList;
+    }
+  }
+
+  function handleLoadMore() {
+    setPage(page + 1);
   }
 
   return (
     <div>
       <h2>Card List:</h2>
-      <ul>{renderCards()}</ul>
+      <ul className="CardList">{renderCards()}</ul>
+      <button onClick={handleLoadMore}>Load More</button>
     </div>
   );
 }
